06db162969c7886da4499e41144fac43a42180f8
Saddam Husein <saddambinhusein2@gmail.com>
2023-09-09 20:06:30 +0800

    day 5.6: react fundamental (useState hook)

--- Day 5/start-learn/src/App.jsx
+++ Day 5/start-learn/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from './components/Button';
 import Text from './components/Text';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <Button class="button">
-        <strong>click me!</strong>
-      </Button>
-      <Text>number</Text>
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
+
+      <Text>{count}</Text>
     </>
   );
 };
