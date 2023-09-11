00df73d187f7ff99dd3cd8a6327aff0d6edf3aa5
Saddam Husein <saddambinhusein2@gmail.com>
2023-09-11 22:47:31 +0800

    day 5.8: react fundamental (react lifecycle in function component: useEffect dependency array)

--- Day 5/start-learn/src/App.jsx
+++ Day 5/start-learn/src/App.jsx
@@ -5,19 +5,26 @@ import Text from './components/Text';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
   };
 
+  const increment2 = () => {
+    setCount2(count2 + 1);
+  };
+
   useEffect(() => {
     console.log('effect');
-  });
+  }, [count]); // only applies to count state
 
   return (
     <>
       <button onClick={increment}>Click me!</button>
       <p>Counter P: {count}</p>
+      <button onClick={increment2}>Click me 2!</button>
+      <p>Counter P2: {count2}</p>
       <Text></Text>
     </>
   );
