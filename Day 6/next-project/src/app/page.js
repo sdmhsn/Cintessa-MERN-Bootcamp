d80e25458c423003997148c3e7410409cabfcb8d
Saddam Husein <saddambinhusein2@gmail.com>
2023-09-12 12:56:48 +0800

    day 6.2: inline styling 2

--- Day 6/next-project/src/app/page.js
+++ Day 6/next-project/src/app/page.js
@@ -2,17 +2,17 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
+  const mainStyle = {
+    color: 'red',
+    background: 'blue',
+    border: '1px solid #000',
+    width: 100,
+    height: '20em',
+  };
+
   return (
     <>
-      <div
-        style={{
-          color: 'red',
-          background: 'blue',
-          border: '1px solid #000',
-          width: 100,
-          height: '20em',
-        }}
-      ></div>
+      <div style={mainStyle}></div>
     </>
   );
 }
