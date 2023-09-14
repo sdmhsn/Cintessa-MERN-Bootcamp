8acf12da1897f993689c97ac8304d87829acbb8f
Saddam Husein <saddambinhusein2@gmail.com>
2023-09-14 23:29:11 +0800

    day 7.1: create project

---
+++ Day 7/next-project/src/app/page.js
@@ -0,0 +1,23 @@
+import './main.css';
+import style from './text.module.css';
+import Button from '@/components/Button';
+import '@/styles/master.css';
+
+export default function Home() {
+  const mainStyle = {
+    background: 'blue',
+    border: '1px solid #000',
+    width: 100,
+    height: '20em',
+  };
+
+  return (
+    <>
+      <div style={mainStyle}>
+        <h3 className="textRed">Halo</h3>
+        <h3 className={style.textRed}>Halo</h3>
+      </div>
+      <Button />
+    </>
+  );
+}
