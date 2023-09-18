f6834ec59edad65d6fcb031f43143838699bab8c
Saddam Husein <saddambinhusein2@gmail.com>
2023-09-18 20:16:08 +0800

    day 7.3: customize ant design button (default)

--- Day 7/next-project/src/app/page.js
+++ Day 7/next-project/src/app/page.js
@@ -5,6 +5,8 @@ import { Button, ConfigProvider } from 'antd';
 import theme from '@/utils/themeConfig';
 // import theme from '../utils/themeConfig'; // this also worked
 
+import '@/styles/master.css';
+
 export default function Home() {
   return (
     <>
--- Day 7/next-project/src/utils/themeConfig.js
+++ Day 7/next-project/src/utils/themeConfig.js
@@ -1,7 +1,16 @@
 const themeConfig = {
   token: {
     fontSize: 16,
-    colorPrimary: '#52c41a',
+    colorPrimary: '#018DF0',
+  },
+  components: {
+    Button: {
+      borderRadius: 0,
+      controlHeight: 50,
+      colorPrimary: '#4640DE',
+      colorPrimaryHover: '#CCCCF5',
+      fontSize: 16,
+    },
   },
 };
 
