760fc1b8934668fe8f88da3f2962bc3164315375
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-03 10:14:41 +0800

    day 9.2: express routing (creating express get request method handler) 2

--- Day 9/backend/index.js
+++ Day 9/backend/index.js
@@ -4,7 +4,12 @@ const port = 3000;
 
 // handling GET request to a root path ('/')
 app.get('/', (req, res) => {
-  res.send('Hai! Welcome to Home page');
+  // res.send('Hai! Welcome to Home page');
+  const data = {
+    method: 'GET',
+  };
+
+  res.send(data);
 });
 
 app.listen(port, () => {
