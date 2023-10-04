b8fc0de8d271cf3e1fbd0b6556f05733a49923d9
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-04 15:32:47 +0800

    day 10.6: express controller (creating user.js controller (video code))

--- Day 10/backend/controller/user.js
+++ Day 10/backend/controller/user.js
@@ -1,9 +1,7 @@
-const signin = async (req, res) => {
+export const signin = async (req, res) => {
   res.send('is signin');
 };
 
-const register = async (req, res) => {
+export const register = async (req, res) => {
   res.send('is register');
 };
-
-export default { signin, register };
