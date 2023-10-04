82253cc4026b6d0e2db6c8f950e61638acdc9107
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-04 11:04:17 +0800

    day 10.3: mongodb (connecting express to mongodb using mongoose)

--- Day 10/backend/server.js
+++ Day 10/backend/server.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
+
+// mongodb connection
+mongoose.connect(
+  'mongodb://root:123456@127.0.0.1:27017/jobhunt?authSource=admin'
+);
+const db = mongoose.connection;
+/* mongoose connection error & success checking */
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('connection success'));
 
 app.listen(port, () => {
   console.log(`Server running in port: ${port}`);
