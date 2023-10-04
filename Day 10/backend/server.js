0128a7731bd517204ea771510145b45e04d2ecad
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-04 11:49:30 +0800

    day 10.5: express (adding auth.js router into server.js)

--- Day 10/backend/server.js
+++ Day 10/backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import authRouter from './router/auth.js'; // using ES6 should include .js extension
+
 const app = express();
 const port = 3000;
 
@@ -12,6 +14,9 @@ const db = mongoose.connection;
 /* mongoose connection error & success checking */
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('connection success'));
+
+// middleware
+app.use('/', authRouter);
 
 app.listen(port, () => {
   console.log(`Server running in port: ${port}`);
