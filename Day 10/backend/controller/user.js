ec27cd296a94606540fa3e7cc0b60d5642d9b036
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-04 20:20:27 +0800

    day 10.7: express model (creating new connected register user controller to User model)

--- Day 10/backend/controller/user.js
+++ Day 10/backend/controller/user.js
@@ -1,7 +1,14 @@
+import User from '../model/User.js';
+
 export const signin = async (req, res) => {
   res.send('is signin');
 };
 
 export const register = async (req, res) => {
-  res.send('is register');
+  const { name, email, password } = req.body;
+
+  const user = await User.create({ name, email, password });
+  user.save();
+
+  res.send({ message: 'Register Success' });
 };
