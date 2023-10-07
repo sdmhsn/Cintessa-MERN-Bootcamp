9d4267ed42db68661054e8175db67f68150c0886
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-07 18:24:03 +0800

    day 10.9: express jwt (adding and using jwt into signin controller to create a token for sign in user)

--- Day 10/backend/controller/user.js
+++ Day 10/backend/controller/user.js
@@ -2,32 +2,32 @@ import User from '../model/User.js';
 
 import bcrypt from 'bcrypt';
 
+import jwt from 'jsonwebtoken';
+
 export const signin = async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email: email }).exec();
-  console.log(user);
-  /*
-    sample of user output:
-    {
-      _id: new ObjectId("652029b32986538624f641f8"),
-      name: 'Tati Husein',
-      email: 'tati@gmail.com',
-      password: '$2b$10$uS.M7gu/BgEIp4JthNwRBuFtxF0HcUxVuQgGukB/DifzfUIgaay56',
-      __v: 0
-    }
-  */
 
   if (user) {
     const compare = bcrypt.compareSync(password, user.password);
-    // console.log(compare); // output: true / false
 
     if (compare) {
-      return res.send({ message: 'Sign in Success' });
+      const token = jwt.sign(
+        {
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+          data: user.id,
+        },
+        'secret'
+      );
+
+      return res.send({ message: 'Sign in Success', token: token });
     }
+
     return res.send({ message: 'Sign in Failed' });
   }
-  res.send('Email or Password not found!');
+
+  return res.send('Email or Password not found!');
 };
 
 export const register = async (req, res) => {
