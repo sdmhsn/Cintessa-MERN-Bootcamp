ea4a985fd4cdc861e7cdee369819b0a2a08b68f5
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-07 09:27:56 +0800

    day 10.8: express controller (creating new sign in controller (user input pasword and encrypt password checking))

--- Day 10/backend/controller/user.js
+++ Day 10/backend/controller/user.js
@@ -1,7 +1,33 @@
 import User from '../model/User.js';
 
+import bcrypt from 'bcrypt';
+
 export const signin = async (req, res) => {
-  res.send('is signin');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email: email }).exec();
+  console.log(user);
+  /*
+    sample of user output:
+    {
+      _id: new ObjectId("652029b32986538624f641f8"),
+      name: 'Tati Husein',
+      email: 'tati@gmail.com',
+      password: '$2b$10$uS.M7gu/BgEIp4JthNwRBuFtxF0HcUxVuQgGukB/DifzfUIgaay56',
+      __v: 0
+    }
+  */
+
+  if (user) {
+    const compare = bcrypt.compareSync(password, user.password);
+    // console.log(compare); // output: true / false
+
+    if (compare) {
+      return res.send({ message: 'Sign in Success' });
+    }
+    return res.send({ message: 'Sign in Failed' });
+  }
+  res.send('Email or Password not found!');
 };
 
 export const register = async (req, res) => {
