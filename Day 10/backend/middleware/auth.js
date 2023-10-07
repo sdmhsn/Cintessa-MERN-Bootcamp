169931fd4ec6b200b7482d9c0aa7a3120a88a862
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-07 20:54:32 +0800

    day 10.9: express jwt (auth.js middleware - creating getToken() for token availability checking in header)

---
+++ Day 10/backend/middleware/auth.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+
+// token availability checking:
+const getToken = (req) => {
+  let token = req.headers.authorization
+    ? req.headers.authorization.replace('Bearer ', '')
+    : null;
+
+  return token && token.length ? token : null;
+};
