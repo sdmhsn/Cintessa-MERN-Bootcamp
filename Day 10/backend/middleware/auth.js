7c7a0672b0cdfb35307780c72cda9156540512c8
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-08 11:52:55 +0800

    day 10.9: express jwt (auth.js middleware - creating decodeToken() using jwt.verify() to decode token from  getToken())

--- Day 10/backend/middleware/auth.js
+++ Day 10/backend/middleware/auth.js
@@ -8,3 +8,20 @@ const getToken = (req) => {
 
   return token && token.length ? token : null;
 };
+
+// decoding getToken()
+export const decodeToken = async (req, res, next) => {
+  try {
+    let token = getToken(req);
+
+    if (!token) {
+      return res.send({ message: 'Authentication failed' }); // stop token decoding if getToken() return null (no auth)
+    }
+
+    // token available
+    var verifyToken = jwt.verify(token, 'secret'); // the secret key ('secret') in jwt.verify(), should be correspond with secret key in  jwt.sign()
+    console.log(verifyToken); // output: { exp: 1696675808, data: '6520b01fc382a2c8a5b7d928', iat: 1696672208 }
+  } catch (err) {
+    console.log(err); // err: invalid token
+  }
+};
