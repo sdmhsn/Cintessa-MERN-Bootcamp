5d384d76a68c18e8c51a9e3f9eb2a3cb44e0c3bf
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-09 12:55:01 +0800

    day 10.9: express jwt (modifying decodeToken() auth middleware)

--- Day 10/backend/middleware/auth.js
+++ Day 10/backend/middleware/auth.js
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
 
+import User from '../model/User.js';
+
 // token availability checking:
-const getToken = (req) => {
+export const getToken = (req) => {
   let token = req.headers.authorization
     ? req.headers.authorization.replace('Bearer ', '')
     : null;
@@ -20,8 +22,27 @@ export const decodeToken = async (req, res, next) => {
 
     // token available
     var verifyToken = jwt.verify(token, 'secret'); // the secret key ('secret') in jwt.verify(), should be correspond with secret key in  jwt.sign()
-    console.log(verifyToken); // output: { exp: 1696675808, data: '6520b01fc382a2c8a5b7d928', iat: 1696672208 }
+    // console.log(verifyToken); // output: { exp: 1696675808, data: '6520b01fc382a2c8a5b7d928', iat: 1696672208 }
+
+    const user = await User.findById(verifyToken.data).exec();
+    /* 
+      user output:
+      {
+        _id: new ObjectId("6520b01fc382a2c8a5b7d928"),
+        name: 'Saddam Husein',
+        email: 'saddam@gmail.com',
+        password: '$2b$10$QU2sQRoZfwm.dOWzmDdDO.dtN5YeezLr0yrHHHe7nAz3B6YTamUqi',
+        __v: 0
+      }
+    */
+
+    if (user) {
+      return next(); // return next(): continue to return res.send({ message: 'Profile' }). this also to stop loading in postman
+    }
+
+    return res.send({ message: 'Authentication failed' });
   } catch (err) {
-    console.log(err); // err: invalid token
+    // console.log(err); // err: invalid token
+    return res.send({ message: err.message });
   }
 };
