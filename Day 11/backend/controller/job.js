1daad6507e7249219be9eb35d98be729357ac26b
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-12 09:03:36 +0800

    day 11.2: express controller (creating createJob() in job controller [POST])

---
+++ Day 11/backend/controller/job.js
@@ -0,0 +1,22 @@
+import Job from '../model/Job.js';
+
+export const createJob = async (req, res) => {
+  /*
+    Sample 1:
+    const job = await Job({
+      name: req.body.name,
+      agency: req.body.agency,
+      category: req.body.category,
+      skill: req.body.skill,
+    });
+    await job.save();
+  */
+
+  /* Sample 2: */
+  const payload = req.body;
+  // console.log(payload);
+  const job = await Job(payload);
+  await job.save();
+
+  return res.send({ message: 'Job created', data: job });
+};
