130ce356b58aaed835b73241fa25c5d6dcbcc542
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-12 09:09:37 +0800

    day 11.2: express router (creating create job router in private.js router)

--- Day 11/backend/router/private.js
+++ Day 11/backend/router/private.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
 import { profile } from '../controller/user.js';
-import { myApplication } from '../controller/application.js';
+import { createJob } from '../controller/job.js';
 
 const router = express.Router();
 
 router.post('/profile', profile);
-router.post('/my-application', myApplication); // new router still need authentication because we define the private router under the decodeToken() in server.js
+router.post('/create-job', createJob);
 
 export default router;
