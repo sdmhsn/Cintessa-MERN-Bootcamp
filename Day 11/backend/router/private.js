8fcd8ac1de5980c30fadb69fcf240bf495ae8696
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-12 12:53:36 +0800

    day 11.2: express router (creating get job by id router in private.js router)

--- Day 11/backend/router/private.js
+++ Day 11/backend/router/private.js
@@ -5,6 +5,7 @@ import { createJob } from '../controller/job.js';
 import { job } from '../controller/job.js';
 import { updateJob } from '../controller/job.js';
 import { deleteJob } from '../controller/job.js';
+import { getJobById } from '../controller/job.js';
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.post('/create-job', createJob);
 router.get('/job', job);
 router.put('/job/:id', updateJob);
 router.delete('/job/:id', deleteJob);
+router.get('/job/:id', getJobById);
 
 export default router;
