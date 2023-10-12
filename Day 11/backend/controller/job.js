11227c0eaa332aba06f87a8f43eb68025972d0d6
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-12 12:09:31 +0800

    day 11.2: express controller (creating updateJob() in job controller [PUT])

--- Day 11/backend/controller/job.js
+++ Day 11/backend/controller/job.js
@@ -26,3 +26,28 @@ export const job = async (req, res) => {
 
   return res.send(job);
 };
+
+export const updateJob = async (req, res) => {
+  const payload = req.body;
+  // console.log(payload);
+  const id = req.params.id;
+  const jobIdData = await Job.findById(id).exec();
+  // console.log(jobIdData);
+
+  /* Sample 1
+  const jobUpdated = await Job.findOneAndUpdate(jobIdData, {
+    name: req.body.name,
+    agency: req.body.agency,
+    category: req.body.category,
+    skill: req.body.skill,
+  }); // the new data output will automatically show in postman output
+  */
+
+  /* Sample 2 */
+  const jobUpdated = await Job.findOneAndUpdate(jobIdData, payload, {
+    new: true, // if true, return the modified document rather than the original (the new data output will show in postman output, if false the data won't show / still the original data)
+    runValidators: true,
+  });
+
+  return res.send({ message: 'Job updated', data: jobUpdated });
+};
