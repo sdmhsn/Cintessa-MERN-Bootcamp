3df4adf3de16a9b0dfe9ef67efcb2b6dab18fc0a
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-12 12:11:40 +0800

    day 11.2: express router (creating update job router in private.js router)

--- Day 11/backend/router/private.js
+++ Day 11/backend/router/private.js
@@ -3,11 +3,13 @@ import express from 'express';
 import { profile } from '../controller/user.js';
 import { createJob } from '../controller/job.js';
 import { job } from '../controller/job.js';
+import { updateJob } from '../controller/job.js';
 
 const router = express.Router();
 
 router.post('/profile', profile);
 router.post('/create-job', createJob);
 router.get('/job', job);
+router.put('/job/:id', updateJob);
 
 export default router;
