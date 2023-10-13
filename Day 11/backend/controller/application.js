60497e0fdb79005aa064b832962d418bda44b021
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-13 08:42:09 +0800

    day 11.3: express controller (creating createApplication() controller [POST])

--- Day 11/backend/controller/application.js
+++ Day 11/backend/controller/application.js
@@ -1,3 +1,21 @@
+import Application from '../model/Application.js';
+
 export const myApplication = async (req, res, next) => {
   return res.send({ message: 'application' });
+};
+
+export const createApplication = async (req, res) => {
+  const payload = req.body;
+  // console.log(payload);
+  // console.log(req.user);
+  const application = new Application({
+    user: req.user._id, // req.user._id is get from req.user = user in decodeToken() auth middleware
+    additional_information: req.body.additional_information
+      ? req.body.addition_information
+      : '',
+    ...payload,
+  });
+  await application.save();
+
+  return res.send({ message: 'Application created', data: application });
 };
