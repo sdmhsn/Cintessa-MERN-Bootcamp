b19aff0075ffbf01978d9a6fcd4d586322a32038
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-27 00:25:45 +0800

    day 14.6: separating layout code to create a new dynamic template layout for user

--- Day 14/next-project/src/app/user/page.js
+++ Day 14/next-project/src/app/user/page.js
@@ -2,27 +2,14 @@
 
 import React from 'react';
 
-import { Layout } from 'antd';
-
 import './index.css';
-import SideBar from '@/components/organisms/Sidebar';
-import HeaderCustom from '@/components/organisms/Header';
 import DashboardContent from './DashboardContent';
-
-const { Footer, Content } = Layout;
+import UserLayout from '@/components/templates/Layout';
 
 export default function page() {
   return (
-    <>
-      <Layout className="layout-wrapper">
-        <SideBar />
-        <Layout>
-          <HeaderCustom />
-          <Content className="content">
-            <DashboardContent />
-          </Content>
-        </Layout>
-      </Layout>
-    </>
+    <UserLayout>
+      <DashboardContent />
+    </UserLayout>
   );
 }
