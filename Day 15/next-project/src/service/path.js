bfb5a1836f897c0df3fc0f3fc65c4b76e45fd28a
Saddam Husein <saddambinhusein2@gmail.com>
2023-10-30 23:52:20 +0800

    day 15.2: register/signup (creating register_path in service/path.js)

--- Day 15/next-project/src/service/path.js
+++ Day 15/next-project/src/service/path.js
@@ -1,4 +1,6 @@
-const base_url = 'http://localhost:3007'; // backend api
+const base_url = 'http://localhost:8000'; // backend api
 
 export const get_blog = base_url + '/';
 export const get_detail_blog = base_url + '/detail';
+
+export const register_path = base_url + '/register';
