a8f07dbdbbc64aa5afc48c048ddbfbdaf8afcfdc
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-07 13:09:37 +0800

    15.5: login/signin (creating signin_path in service/path.js)

--- Day 15/next-project/src/service/path.js
+++ Day 15/next-project/src/service/path.js
@@ -4,3 +4,4 @@ export const get_blog = base_url + '/';
 export const get_detail_blog = base_url + '/detail';
 
 export const register_path = base_url + '/register';
+export const signin_path = base_url + '/signin';
