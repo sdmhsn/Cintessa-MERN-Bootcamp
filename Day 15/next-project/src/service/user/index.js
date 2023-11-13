6291b19faca5829ca1f5553a511f6da715eeb624
Saddam Husein <saddambinhusein2@gmail.com>
2023-11-13 13:40:16 +0800

    15.5: login/signin (creating serviceSignin with post method for user service using axios)

--- Day 15/next-project/src/service/user/index.js
+++ Day 15/next-project/src/service/user/index.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 
-import { register_path } from '../path';
+import { register_path, signin_path } from '../path';
 
 export const serviceRegister = async (payload) => {
   try {
     const response = await axios.post(register_path, payload);
     // console.log(response);
+    return response;
+  } catch (err) {
+    // console.log(err.message);
+    return err;
+  }
+};
+
+export const serviceSignin = async (payload) => {
+  try {
+    const response = await axios.post(signin_path, payload);
+
     return response;
   } catch (err) {
     // console.log(err.message);
